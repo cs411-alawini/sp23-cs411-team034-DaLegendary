e39879468db7fa5b7b7956622c1fcee55486fd9a
YU-Anthony <zhang.yuanthony@gmail.com>
2023-04-10 22:49:34 -0500

    update README

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,8 +4,14 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:3000', // the address and port for the current react app
+      target: 'http://127.0.0.1:5000',
       changeOrigin: true,
+      pathRewrite: {
+        '^/api': '',
+      },
+      onProxyRes: function(proxyRes, req, res) {
+        proxyRes.headers['Access-Control-Allow-Origin'] = 'http://127.0.0.1:3000';
+      },
     })
   );
 };
